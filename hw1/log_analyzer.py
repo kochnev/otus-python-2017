95cd79a486697bd02402323c08948760be3dbd52
Alexander <lyotchik1@mail.ru>
2017-12-19 10:47:19 +0300

    attemp 2

--- hw1/log_analyzer.py
+++ hw1/log_analyzer.py
@@ -2,28 +2,28 @@
 # -*- coding: utf-8 -*-
 
 
-# log_format ui_short '$remote_addr $remote_user $http_x_real_ip [$time_local] "$request" '
-#                     '$status $body_bytes_sent "$http_referer" '
-#                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_USER" '
-#                     '$request_time';
+# log_format ui_short
+#     '$remote_addr $remote_user $http_x_real_ip [$time_local] "$request" '
+#     '$status $body_bytes_sent "$http_referer" '
+#     '"$http_user_agent" "$http_x_forwarded_for"'
+#     '"$http_X_REQUEST_ID" "$http_X_USER" '
+#     '$request_time';
 
 import os
 import sys
-import fnmatch
 import gzip
 import re
 import json
 import time
 import logging
 import argparse
+import shutil
 from collections import defaultdict, namedtuple
 from datetime import datetime as dt
 from statistics import median
 
-
-
 primary_config = {
-    "REPORT_SIZE": 500,
+    "REPORT_SIZE": 100,
     "REPORT_DIR": "./reports",
     "LOG_DIR": "./log",
     "TS_DIR": "./ts",
@@ -34,24 +34,36 @@ config_path = '/usr/local/etc/log_analyzer.conf'
 
 LogInfo = namedtuple('LogInfo', 'date, path')
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+JS_NAME = 'jquery.tablesorter.min.js'
 
-def create_report_html(report_data, report_file_path):
+
+def create_report_html(report_data, report_dir, report_name):
     """function to save report in html"""
     try:
         with open('report.html', 'r', encoding='utf-8') as template:
             template_data = template.read()
-    except:
+    except Exception:
         logging.error("An error occured while opening report.html")
         raise
 
     json_data = json.dumps(report_data)
 
     ready_data = template_data.replace('$table_json', json_data)
+
+    if not os.path.exists(report_dir):
+        os.makedirs(report_dir)
+
+    if not os.path.exists(os.path.join(report_dir, JS_NAME)):
+        shutil.copy(os.path.join(BASE_DIR, JS_NAME), report_dir)
+
+    report_path = os.path.join(report_dir, report_name)
     try:
-        with open(report_file_path, 'w', encoding='utf-8') as html_report:
+        with open(report_path, 'w', encoding='utf-8') as html_report:
             html_report.write(ready_data)
-    except:
-        logging.error("An error occured while opening {0}".format(report_file_path))
+    except Exception:
+        logging.error("An error occured while opening {0}".
+                      format(report_path))
         raise
 
 
@@ -69,52 +81,57 @@ def get_report_data(config, parsed_lines):
     table = []
     for url, time_col in grouped_urls.items():
         time_sum = sum(time_col)
-        if time_sum > report_size:
-            count = len(time_col)
-            count_p = (count / all_count) * 100
-            time_avg = time_sum / count
-            time_max = max(time_col)
-            time_med = median(time_col)
-            time_perc = (time_sum / all_count) * 1
+        count = len(time_col)
+        time_avg = time_sum / count
+        count_p = (count / all_count) * 100
+        time_max = max(time_col)
+        time_med = median(time_col)
+        time_perc = (time_sum / all_count) * 1
 
-            row = {
-                "url": url,
-                "count": count,
-                "count_perc": round(count_p, 3),
-                "time_avg": round(time_avg, 3),
-                "time_max": round(time_max, 3),
-                "time_med": round(time_med, 3),
-                "time_perc": round(time_perc, 3),
-                "time_sum": round(time_sum, 3)
-            }
+        row = {
+            "url": url,
+            "count": count,
+            "count_perc": round(count_p, 3),
+            "time_avg": round(time_avg, 3),
+            "time_max": round(time_max, 3),
+            "time_med": round(time_med, 3),
+            "time_perc": round(time_perc, 3),
+            "time_sum": round(time_sum, 3)
+        }
 
-            table.append(row)
+        table.append(row)
 
-    return sorted(table, key=lambda k: k['time_sum'], reverse=True)
+    sorted_table = sorted(table, key=lambda k: k['time_sum'], reverse=True)
+
+    return sorted_table[:report_size]
 
 
 def parse_log(config, lines):
     """generator to get tuples(logdate,logpath) by splitting lines"""
     threshold_err = config["THRESHOLD_ERR"]
     total = error = 0
-    patc = re.compile('.+"(GET|POST)\s(?P<url>\S+)\sHTTP.+(?P<t_request>\d+\.\d+)$')
+    pat = '.+"(GET|POST)\s(?P<url>\S+)\sHTTP.+(?P<t_request>\d+\.\d+)$'
+    patc = re.compile(pat)
 
     for line in lines:
-        total+=1
+        total += 1
         try:
             m = patc.search(line)
             if not m:
-                raise
+                raise Exception
             url = m.group('url')
             r_time = float(m.group('t_request'))
             yield url, r_time
-        except:
-            error+=1
-    
-    rate = error/float(total)
+        except Exception:
+            error += 1
+
+    ftotal = float(total)
+    rate = error / ftotal
     if rate > threshold_err:
-        logging.error("Percent of lines parsed with errors exceed the threshold")
+        logging.error("Percent of lines parsed \
+                            with errors exceed the threshold")
         sys.exit()
+
 
 def read_log(log_path):
     """generator for reading lines of logfile"""
@@ -148,11 +165,15 @@ def update_ts(config):
     ts_dir = config["TS_DIR"]
     file_path = os.path.join(ts_dir, "log_analyzer.ts")
 
+    if not os.path.exists(ts_dir):
+        os.makedirs(ts_dir)
+
     try:
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(ts_str)
-    except:
-        logging.error("An error occured while opening {0}".format(report_file_path))
+    except Exception:
+        logging.error("An error occured while opening {0}"
+                      .format(file_path))
         raise
 
     os.utime(file_path, (int(ts), int(ts)))
@@ -169,22 +190,21 @@ def get_config_dict(path_to_config):
 
 def check_config(config):
     try:
-        report_size = int(config["REPORT_SIZE"]),
-        report_dir = config["REPORT_DIR"],
-        log_dir = config["LOG_DIR"],
-        ts_dir = config["TS_DIR"],
-    except:
+        int(config["REPORT_SIZE"])
+        config["REPORT_DIR"]
+        config["LOG_DIR"]
+        config["TS_DIR"]
+    except Exception:
         logging.error("please, check your config file")
         raise Exception("please, check your config file")
 
 
 def main(config):
     logging.info("search for logfiles")
-    
     log_folder = config["LOG_DIR"]
     if not os.path.exists(log_folder):
         logging.error("Folder {} does not exist!".format(log_folder))
-        return 
+        return
 
     logfiles = get_logfiles(log_folder)
 
@@ -192,21 +212,22 @@ def main(config):
     latest_logfile = max(logfiles, key=lambda log: log.date)
 
     logging.info("get the attributes of the latest logfile")
-    latest_logfile_name = latest_logfile[1]
-    latest_log_day = latest_logfile[0]
-    logging.info("the latest log file is {0}".format(latest_logfile_name))
+    logging.info("the latest log file is {0}".format(latest_logfile.path))
 
-    report_name = "report_{0}.html".format(latest_log_day)
+    report_name = "report_{0}.html".format(latest_logfile.date)
     report_dir = config["REPORT_DIR"]
-    report_file_path = os.path.join(report_dir, report_name)
-    logging.info("report name is {0}".format(report_file_path))
+    report_path = os.path.join(report_dir, report_name)
 
-    if os.path.exists(report_file_path):
-        logging.info("Report for date  {0} already exists".format(latest_log_day))
+    logging.info("report name is {0}".format(report_path))
+
+    if os.path.exists(report_path):
+        logging.info("Report for date  {0} already exists"
+                     .format(latest_logfile.date))
+        update_ts(config)
         return
 
     logging.info("read lines from the log file")
-    lines = read_log(latest_logfile_name)
+    lines = read_log(latest_logfile.path)
 
     logging.info("get parsed lines")
     parsed_lines = parse_log(config, lines)
@@ -215,7 +236,7 @@ def main(config):
     report_data = get_report_data(config, parsed_lines)
 
     logging.info("generating html report...")
-    create_report_html(report_data, report_file_path)
+    create_report_html(report_data, report_dir, report_name)
     logging.info("report html has created successfully!")
     update_ts(config)
     logging.info("timestamp has updated")
@@ -234,7 +255,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     arg_config = get_config_dict(args.config)
-    
+
     primary_config.update(arg_config)
 
     check_config(primary_config)
