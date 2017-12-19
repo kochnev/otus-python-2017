e18f48cbb7ada320e67511078bcabc896d929182
Alexander <lyotchik1@mail.ru>
2017-12-19 17:02:27 +0300

    basic functionality without OOP and validations

--- hw3/api.py
+++ hw3/api.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import abc
+# import abc
 import json
 import datetime
 import logging
 import hashlib
 import uuid
+import scoring
 from optparse import OptionParser
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
@@ -36,11 +37,23 @@ GENDERS = {
 }
 
 
-class CharField(object):
+class Field(object):
+    #  __metaclass__ = abc.ABCMeta
+
+    def __init__(self, required, nullable=False):
+        self.required = required
+        self.nullable = nullable
+
+    # @abc.abstractproperty
+    # def value(self):
+    #     return
+
+
+class CharField(Field):
     pass
 
 
-class ArgumentsField(object):
+class ArgumentsField(Field):
     pass
 
 
@@ -48,30 +61,33 @@ class EmailField(CharField):
     pass
 
 
-class PhoneField(object):
+class PhoneField(Field):
     pass
 
 
-class DateField(object):
+class DateField(Field):
     pass
 
 
-class BirthDayField(object):
+class BirthDayField(Field):
     pass
 
 
-class GenderField(object):
+class GenderField(Field):
     pass
 
 
-class ClientIDsField(object):
+class ClientIDsField(Field):
     pass
 
 
 class ClientsInterestsRequest(object):
     client_ids = ClientIDsField(required=True)
     date = DateField(required=False, nullable=True)
-
+    
+    def __init__(self, **kwargs):
+        for key, value in kwargs.iteritems():
+            setattr(self, key, value)
 
 class OnlineScoreRequest(object):
     first_name = CharField(required=False, nullable=True)
@@ -81,6 +97,10 @@ class OnlineScoreRequest(object):
     birthday = BirthDayField(required=False, nullable=True)
     gender = GenderField(required=False, nullable=True)
 
+    def __init__(self, **kwargs):
+        for key, value in kwargs.iteritems():
+            setattr(self, key, value)
+
 
 class MethodRequest(object):
     account = CharField(required=False, nullable=True)
@@ -89,6 +109,10 @@ class MethodRequest(object):
     arguments = ArgumentsField(required=True, nullable=True)
     method = CharField(required=True, nullable=False)
 
+    def __init__(self, **kwargs):
+        for key, value in kwargs.iteritems():
+            setattr(self, key, value)
+
     @property
     def is_admin(self):
         return self.login == ADMIN_LOGIN
@@ -96,16 +120,37 @@ class MethodRequest(object):
 
 def check_auth(request):
     if request.login == ADMIN_LOGIN:
-        digest = hashlib.sha512(datetime.datetime.now().strftime("%Y%m%d%H") + ADMIN_SALT).hexdigest()
+        digest = hashlib.sha512(datetime.datetime.now().strftime("%Y%m%d%H") /
+                                + ADMIN_SALT).hexdigest()
     else:
-        digest = hashlib.sha512(request.account + request.login + SALT).hexdigest()
+        digest = hashlib.sha512(request.account /
+                                + request.login + SALT).hexdigest()
     if digest == request.token:
         return True
     return False
 
 
 def method_handler(request, ctx, store):
-    response, code = None, None
+    # response, code = None, None
+    # return response, code
+    response, code = {}, OK
+
+    logging.info("method_handler! "+str(request['body']))
+    mr = MethodRequest(**request['body'])
+    import pdb; pdb.set_trace()
+    if mr.method == "online_score":
+        osr = OnlineScoreRequest(**mr.arguments)
+        score = scoring.get_score(store=None, **mr.arguments)
+        response["score"] = score
+    elif mr.method == "clients_interests":
+        cir = ClientsInterestsRequest(**mr.arguments)
+        for cid in cir.client_ids:
+            interests = scoring.get_interests(None, cid)
+            response[cid] = interests
+    else:
+        code = BAD_REQUEST
+
+           
     return response, code
 
 
@@ -124,17 +169,22 @@ class MainHTTPHandler(BaseHTTPRequestHandler):
         request = None
         try:
             data_string = self.rfile.read(int(self.headers['Content-Length']))
+            logging.info("data_string! "+data_string)
             request = json.loads(data_string)
-        except:
+        except Exception:
             code = BAD_REQUEST
-
         if request:
             path = self.path.strip("/")
-            logging.info("%s: %s %s" % (self.path, data_string, context["request_id"]))
+            logging.info("%s: %s %s" %
+                         (self.path, data_string, context["request_id"]))
             if path in self.router:
                 try:
-                    response, code = self.router[path]({"body": request, "headers": self.headers}, context, self.store)
-                except Exception, e:
+                    response, code = self.router[path]({
+                                                        "body": request,
+                                                        "headers": self.headers
+                                                       },
+                                                       context, self.store)
+                except Exception as e:
                     logging.exception("Unexpected error: %s" % e)
                     code = INTERNAL_ERROR
             else:
@@ -146,11 +196,13 @@ class MainHTTPHandler(BaseHTTPRequestHandler):
         if code not in ERRORS:
             r = {"response": response, "code": code}
         else:
-            r = {"error": response or ERRORS.get(code, "Unknown Error"), "code": code}
+            r = {"error": response or ERRORS.get(code, "Unknown Error"),
+                 "code": code}
         context.update(r)
         logging.info(context)
         self.wfile.write(json.dumps(r))
         return
+
 
 if __name__ == "__main__":
     op = OptionParser()
@@ -158,7 +210,8 @@ if __name__ == "__main__":
     op.add_option("-l", "--log", action="store", default=None)
     (opts, args) = op.parse_args()
     logging.basicConfig(filename=opts.log, level=logging.INFO,
-                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+                        format='[%(asctime)s] %(levelname).1s %(message)s',
+                        datefmt='%Y.%m.%d %H:%M:%S')
     server = HTTPServer(("localhost", opts.port), MainHTTPHandler)
     logging.info("Starting server at %s" % opts.port)
     try:
